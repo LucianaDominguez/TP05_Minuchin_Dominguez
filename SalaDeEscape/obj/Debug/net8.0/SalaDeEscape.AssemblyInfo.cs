77ce6db2de948dc3e6b2532e7714be8c217e8e43
LUCIANA MICAELA DOMINGUEZ SZTERN <48039458@ort>
2024-07-12 15:38:58 -0300

    css habitaciones 2

--- SalaDeEscape/obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
+++ SalaDeEscape/obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4123e8212c3896d0ff16bd290ce9f2346f2f012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7072e02153fe91a814333972f8f5cd88acb8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
